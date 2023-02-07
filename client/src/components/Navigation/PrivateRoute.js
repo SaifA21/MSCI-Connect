0e30a940ca48ea3f40bba151450bfb6538d686ae
Saif Abuosba <48367948+SaifA21@users.noreply.github.com>
2023-02-07 00:43:54 -0500

    Logic for which component to choose upon entering website added

--- client/src/components/Navigation/PrivateRoute.js
+++ client/src/components/Navigation/PrivateRoute.js
@@ -1,27 +1,43 @@
 import React from "react";
 import { Router, Switch, Route } from "react-router-dom";
-import Chat from "../chat";
+import Chat from "../Chat";
 import FAQ from "../FAQ";
+import SignIn from "../SignIn";
 import Home from '../Home';
 import LookUp from "../LookUp";
 import Polls from "../Polls";
-import SignIn from "../SignIn";
 import SignOut from "../SignOut";
 import SignUp from "../SignUp";
 import TC from "../TC";
 import Timeline from "../Timeline";
 import history from './history';
+import { useAuth } from "../../contexts/AuthContext";
 
 export default function PrivateRoute({
-  //authenticated,
-  //...rest
+  
 }) {
+
+
+  const{currentUser} = useAuth()
+
+  var defaultPage;
+
+  if(currentUser == null){
+    defaultPage = SignIn
+  }
+
+  else{
+    
+    defaultPage = Home}
+
+
+
   return (
 
     <Router history={history}>
       <Switch>
       <Route path="/signup" exact component={SignUp} />
-      <Route exact path="/home"  component={Home} />
+      <Route path="/home"  component={Home} />
       <Route path="/signin" exact component={SignIn} />
       <Route path="/lookup" exact component={LookUp} />
       <Route path="/chat" exact component={Chat} />
@@ -30,6 +46,7 @@ export default function PrivateRoute({
       <Route path="/tc" exact component={TC} />
       <Route path="/timeline" exact component={Timeline} />
       <Route path="/signout" exact component={SignOut} />
+      <Route exact path='/' component={defaultPage} />
       </Switch>
     </Router>
   );

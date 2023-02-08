9256bc82b1d212cd2bdbf4ea488bbb9a8313a444
Saif Abuosba <48367948+SaifA21@users.noreply.github.com>
2023-02-08 01:08:20 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
   host    : 'ec2-18-216-101-119.us-east-2.compute.amazonaws.com',
-  user    : 'sabuosba',
-  password: 'ZZ12345678z_',
-  database: 'sabuosba'
+  user    : '',
+  password: '',
+  database: ''
 };
  
 module.exports = config;

007785f584f6bbe3d16ebf827cfca1471e521059
sselvaka <sselvaka@uwaterloo.ca>
2023-02-22 22:17:23 -0500

    Finished student lookup

--- client/src/components/LookUp/index.js
+++ client/src/components/LookUp/index.js
@@ -1,18 +1,66 @@
 import React from 'react'
 import { useAuth } from '../../contexts/AuthContext';
 import Navbar from '../Navigation/Navbar.js'
+import { useState } from 'react';
+import { Typography } from '@material-ui/core';
+
+const serverURL = ""; //enable for dev mode
 
 export default function LookUp() {
   const{currentUser} = useAuth()
+
+  const[emails,changeEmails]=useState([
+    {
+      "username":"Thev",
+      "email":"t2nirmal@uwaterloo.ca"
+    }
+  ]);
+
+  React.useEffect(() =>{
+    loadEmails();
+  },[])
+
+  const loadEmails = () => {
+    
+    callApiLoadEmails()
+    .then(res => {
+        var parsed = JSON.parse(res.express);
+        changeEmails(parsed)
+      }
+    ).then(console.log(emails))
+  }
+
+  const callApiLoadEmails = async (props) => {
+    
+    const url = serverURL + "/api/loadEmails";
+    const response = await fetch(url, {method: "POST"});
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  }
+
   return (
     <div>
     {currentUser.uid!=null && (
 
     <div>
         <Navbar></Navbar>
-        <div>Student Lookup</div>
+        <h3>Student LookUp</h3>
+        {emails.map((item)=>{
+        return(
+          <div>
+
+            <br></br>
+      
+            
+            <Typography>{item.username} : {item.email}</Typography>
+            <br></br>
+          </div>
+        )
+      })}
     </div>
     )}
     </div>
   )
 }
+
--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
   host    : 'ec2-18-216-101-119.us-east-2.compute.amazonaws.com',
-  user    : '',
-  password: '',
-  database: ''
+  user    : 'sselvaka',
+  password: '8pkr34je',
+  database: 'sselvaka'
 };
  
 module.exports = config;

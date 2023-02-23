a3ae09f11b40d7077634c2fe98e4243bdacb871a
Theveenan <theveenan@gmail.com>
2023-02-22 22:50:31 -0500

    Fixes for chats done

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
   host    : 'ec2-18-216-101-119.us-east-2.compute.amazonaws.com',
-  user    : '',
-  password: '',
-  database: ''
+  user    : 't2nirmal',
+  password: 'msci245',
+  database: 't2nirmal'
 };
  
 module.exports = config;

e5f3eae59b4116a880fb1cefbe31fca05705d4cf
Theveenan <theveenan@gmail.com>
2023-03-12 22:24:55 -0400

    Finished downvoting

--- client/src/components/Home/UpdateItem/UpdateItem.js
+++ client/src/components/Home/UpdateItem/UpdateItem.js
@@ -34,6 +34,32 @@ const  UpdateItem = (props) => {
       return body;
     }
 
+    const onclickDown = () =>{
+      console.log('clicked')
+      callApiDownvoteUpdate()
+      .then(res => {
+        var parsed = JSON.parse(res.express);
+      })
+  
+    }
+
+    const callApiDownvoteUpdate = async () => {
+
+      const url = serverURL + "/api/downvoteUpdate"
+    
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({updateID: props.updateID})
+    
+      });
+      const body = await response.json();
+      if (response.status != 200) throw Error(body.update);
+      return body;
+    }
+
     return (
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
@@ -58,7 +84,7 @@ const  UpdateItem = (props) => {
         </CardContent>
         <CardActions>
             
-           <IconButton color = 'primary' aria-label="add" onClick={onclickUp}>
+           <IconButton color = 'primary' aria-label="add" onClick={onclickDown}>
               <ThumbDownIcon style={{ fontSize: 20 }}/>
            </IconButton>
           <Button size="small">Learn More</Button>

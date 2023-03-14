54c454a953d51238f027dd131671d9a91a0f1040
Saif Abuosba <48367948+SaifA21@users.noreply.github.com>
2023-03-14 02:23:12 -0400

    Create index.js

---
+++ client/src/components/Reported/index.js
@@ -0,0 +1,34 @@
+import React, {useState } from 'react'
+import { useAuth } from '../../contexts/AuthContext';
+import Navbar from '../Navigation/Navbar.js'
+
+
+
+
+export default function Reported() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

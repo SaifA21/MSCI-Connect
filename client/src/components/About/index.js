d7cb8d2829ab08420290fe9431f2ec5e6421ee8b
sselvaka <sselvaka@uwaterloo.ca>
2023-03-22 19:35:23 -0400

    Created about page

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -12,7 +12,17 @@ export default function About() {
   
       <div>
           <Navbar></Navbar>
-          <h1>About Page</h1>
+          <h2><strong>About</strong></h2>
+          <p>The MSCI CONNECT website is a one stop shop for communication and connection to and between students in the Management Engineering Class of 2025 Cohort. Using this website, students can: receive updates regarding their classes or the university community, post discussions for their classmates to see and respond to, vote on polls, and view a timeline of important events that are coming up in the semester. It is important for all users to remember to be appropriate when using the website because class reps can flag users if necessary, limiting their uses on the website.</p>
+          <h2><strong>News Home</strong></h2>
+          <p>Class reps can post and delete news updates on this page for all users to view whenever they please. Students can upvote or downvote, using the thumbs up or thumbs down button respectively, on each update depending on how helpful the update is. Class reps can use this voting system to determine which updates are more important and can add these specific updates to a pinned list. Students can change the way updates are displayed to fit their needs such as sorting by most voted, oldest to newest, or newest to oldest. They can also use filters such as the class filter to only show updates related to a specific class or the pin filter to only show the updates that have been pinned. Students can also subscribe to a mailing list which would allow class reps to send emails directly to them with updates. </p>
+          <h2><strong>Chat Page</strong></h2>
+          <p>This page is for students to post discussion items where their classmates can help clear their doubts. Viewers can choose to report chat messages if they feel that the content is inappropriate in any way leaving the class reps to review the content and then take it down if necessary. Users can either upvote or downvote the posts depending on how helpful they find the information. The chats can be sorted by occurrence or number of upvotes and can also be filtered according to the related class or publisher of the post. Flagged users will not be able to submit discussion posts.</p>
+          <h2><strong>Polls</strong></h2>
+          <p>On this page, class reps can create polls for students to vote on. This is helpful to gather the opinion of the cohort and come to decisions as a group. Class reps can also pin certain polls that they consider to be more important. All users can filter the polls to show the pinned ones and unless a user has been flagged by the reps, all users can vote.</p>
+          <h2><strong>Student Lookup</strong></h2>
+          <p>This page provides the users with a list of every user and their email in case they need to be contacted. Students can also report their classmates if they feel the need to do so.</p>
+          
       </div>
       )}
       </div>
